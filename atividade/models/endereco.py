b15a1afc54c35affa88206feb2ffa1cbb99e24e0
Pedro Lago <159453815+PedroLagoX@users.noreply.github.com>
2024-09-10 22:46:57 +0000

    Python in POO

---
+++ atividade/models/endereco.py
@@ -0,0 +1,21 @@
+from models.enums.unidadefederativa import Unidadefederativa
+
+
+class Endereco:
+    def __init__(self, logradouro:str, numero:str, complemento:str, cep:str, cidade:str, unidadefederativa: Unidadefederativa) -> None:
+        self.logradouro = logradouro
+        self.numero = numero
+        self.complemento = complemento
+        self.cep = cep
+        self.cidade = cidade
+        self.unidadefederativa = unidadefederativa
+
+    def __str__(self) -> str:
+        return (
+                f"\nLogradouro: {self.logradouro}"
+                f"\nNúmero: {self.numero}"
+                f"\nComplemento: {self.complemento}"
+                f"\nCep: {self.cep}"
+                f"\nCidade: {self.cidade}"
+                f"\nUF: {self.unidadefederativa}"
+                )
